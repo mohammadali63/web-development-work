b6feaabffade3414b91991fab05772b491650dfe
mohammadali63 <taheralinur@gmail.com>
2022-10-21 00:30:29 +0600

    work-6 update

--- day-2/work-6/app/classes/HelloWorld.php
+++ day-2/work-6/app/classes/HelloWorld.php
@@ -4,13 +4,14 @@ namespace App\classes;
 
 class HelloWorld
 {
-    public $massage;
-    public function __construct()
+    public $massage,$massage1;
+    public function __construct($data)
     {
         $this->massage='Hello';
+        $this->massage1=$data;
     }
     public function index()
     {
-    echo $this->massage;
+    echo $this->massage.' '.$this->massage1;
     }
 }
