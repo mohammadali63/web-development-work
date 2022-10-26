34b013b0aecb9cf6c467261a83148b1110271a28
mohammadali63 <taheralinur@gmail.com>
2022-10-26 22:30:22 +0600

    While Loop php project

--- day-4/while-loop/app/classes/WhileLoop.php
+++ day-4/while-loop/app/classes/WhileLoop.php
@@ -4,5 +4,21 @@ namespace App\classes;
 
 class WhileLoop
 {
+    public $massage,$i;
+    public function __construct()
+    {
+        $this->massage='Hello World';
+    }
+    public function index()
+    {
+        $this->i=100;
+        while ( $this->i < 110)
+        {
+            echo $this->i,'';
+            $this->i++;
+
+        }
+
+    }
 
 }
