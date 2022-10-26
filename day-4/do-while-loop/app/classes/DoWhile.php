be67d64798c1cbf8f4d2bd371ebbaac559458b2c
mohammadali63 <taheralinur@gmail.com>
2022-10-26 22:21:46 +0600

    Do-while Loop

--- day-4/do-while-loop/app/classes/DoWhile.php
+++ day-4/do-while-loop/app/classes/DoWhile.php
@@ -4,5 +4,21 @@ namespace App\classes;
 
 class DoWhile
 {
+    public $massage,$i;
+    public function __construct()
+    {
+        $this->massage='Hello World';
+    }
+    public function index()
+    {
+        $this->i=20;
+        do
+        {
+            echo $this->i;
+            $this->i++;
+
+        }while($this->i < 25);
+
+    }
 
 }
---
+++ day-4/do-while-loop/index.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once "vendor/autoload.php";
+use App\classes\DoWhile;
+
+$do=new DoWhile();
+$do->index();
