225b394663b6fd542502351f1295f18fefcd4a09
mohammadali63 <taheralinur@gmail.com>
2022-10-26 22:26:55 +0600

    For Loop php project

--- day-4/for-loop/app/classes/ForLoop.php
+++ day-4/for-loop/app/classes/ForLoop.php
@@ -4,5 +4,18 @@ namespace App\classes;
 
 class ForLoop
 {
+    public $massage,$i;
+    public function __construct()
+    {
+        $this->massage="Hello World" ;
+    }
+    public function index()
+    {
+        for ($this->i=30; $this->i >=20; $this->i--)
+        {
+            echo $this->i,'<br/>';
+        }
+
+}
 
 }
---
+++ day-4/for-loop/index.php
@@ -0,0 +1,6 @@
+<?php
+require_once "vendor/autoload.php";
+use App\classes\ForLoop;
+
+$for=new ForLoop();
+$for->index();
