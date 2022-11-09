27234eb2b6e5ee8fc1fdade7392348bad75d0a30
mohammadali63 <taheralinur@gmail.com>
2022-11-09 21:32:35 +0600

    work-1 update

---
+++ day-6-practice/work-1/app/classes/Category.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\classes;
+
+class Category
+{
+    public $categories = [];
+    public function __construct()
+    {
+        $this->categories = [
+            0=>[
+                'íd'    => 1,
+                'name'  => 'National',
+            ],
+            1=>[
+                'id'    => 2,
+                'name'  => 'International'
+            ],
+            2=>[
+                'id'    => 3,
+                'name'  => 'Sports'
+            ],
+            3=>[
+                'id'    => 4,
+                'name'  => 'Economics'
+            ],
+        ];
+
+    }
+    public function getAllCtegory()
+    {
+        return $this->categories;
+    }
+
+
+}
