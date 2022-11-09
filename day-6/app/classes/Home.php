5c24b54630144bc764a3e6f065e015206e2d632c
mohammadali63 <taheralinur@gmail.com>
2022-11-09 18:32:10 +0600

    Day-6 project updated

--- day-6/app/classes/Home.php
+++ day-6/app/classes/Home.php
@@ -1,11 +1,15 @@
 <?php
 
 namespace App\classes;
+use App\classes\Category;
 
 class Home
 {
+    public $category, $categories;
+
     public function index()
     {
-       header('Location: action.php?page=home');
+        header('Location: action.php?page=home');
     }
+
 }
