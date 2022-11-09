cf176cd7976be0fea9c70069b7e97cecdf7ba61e
mohammadali63 <taheralinur@gmail.com>
2022-11-09 23:22:00 +0600

    work-1 action.php file update

--- day-6-practice/work-1/pages/action.php
+++ day-6-practice/work-1/pages/action.php
@@ -1,2 +1,18 @@
 <?php
 
+require_once "vendor/autoload.php";
+
+use App\classes\Category;
+
+$category = new Category();
+
+$categories = $category->getAllCtegory();
+
+if (isset($_GET['page']))
+{
+    if ($_GET['page']=='home')
+    {
+        include 'pages/home.php';
+    }
+}
+
