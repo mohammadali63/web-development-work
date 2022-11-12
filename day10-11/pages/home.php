2357c6093a97199a3775d6f2d8ab70ce8de80b06
mohammadali63 <taheralinur@gmail.com>
2022-11-12 22:05:09 +0600

    Header menu add

--- day10-11/pages/home.php
+++ day10-11/pages/home.php
@@ -6,8 +6,20 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.css"/>
 </head>
 <body>
+    <nav class="navbar navbar-expand">
+        <ul class="navbar-nav">
+            <li><a href="" class="nav-link">Home</a></li>
+            <li><a href="" class="nav-link">About</a></li>
+            <li><a href="" class="nav-link">Service</a></li>
+            <li><a href="" class="nav-link">Contact</a></li>
+        </ul>
+    </nav>
 
+
+
+<script src="assets/js/bootstrap.bundle.js"></script>
 </body>
 </html>
